2304886dc536c5e84db039a1fc20c50365094af0
Heiko Eichhorn <h-speedy@web.de>
2016-05-01 09:22:33 +0200

    First changes

--- task/index.js
+++ task/index.js
@@ -1,1 +1,21 @@
-// JavaScript source code
+/*
+ * grunt-file-rename
+ * https://github.com/devgittry/grunt-file-rename
+ *
+ * Copyright (c) 2016 devgittry
+ * Licensed under the MIT license.
+ * https://github.com/devgittry/grunt-file-rename/blob/master/LICENSE
+ *
+ */
+ 
+'use strict';
+ 
+module.exports = function(grunt) {
+	grunt.registerMultiTask('frename', 'Rename file', function() {
+		var options = this.options({
+      		force: false
+    	});
+
+    	grunt.verbose.writeflags(options, 'Options');
+	});
+};
